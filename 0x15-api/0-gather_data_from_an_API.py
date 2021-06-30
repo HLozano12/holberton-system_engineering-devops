d3d2901465549838f146cdb54b6b4d7788b4151a
HLozano12 <HLR8503@gmail.com>
2021-06-30 17:55:48 +0000

    Rewrite

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
 """Python script that, using this REST API, for a given employee ID,"""
 import requests
-from sys import argv
+import sys
 
 
-if __name__ == "__main__":
+def get_employee_task(employeeId):
+	""" Retrieve employee for API"""
+	# variables
+	name = ''
+	task_list = []
+	completed_counter = 0
 
-    count = 0
-    task_list = []
+	# Get Request
+	userRes = request.get(
+		"https://jsonplaceholder.typicode.com/users/{}".format(employeeId))
+	todosRes = request.get(
+		"https://jsonplaceholder.typicode.com/users/{}/todos".
+		format(employeeId))
 
-    # Get the request from site
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(argv[1]))
-    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                        .format(argv[1]))
+	# json get from responses
+	name = userRes.json().get('name')
+	todosJson = todosRes.json()
 
-    # obtain information from the json response
-    name = user.json().get('name')
-    task = todo.json()
+	# Task loops
+	for task in todosJson:
+		if task.get('completed') is True:
+			completed_counter += 1
+			# This saves title to task_list
+			task_list.append(task.get('title'))
 
-    for task in tasks:
-        if task.get('completed') is True:
-            count += 1
-            task_list.append(task.get('title'))
+	# First line to be printed
+	print('Employee {} is done with task({}/{}):'.format(
+		name, completed_counter, len(todosJson)))
 
-    # Print out the first line
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, count, len(tasks)))
+	# Loop Task list and print the task
+	for title in task_list:
+		print('\t {}'.format(title))
+	return 0
 
-    for task in task_list:
-        print('\t {}'.format(task))
+if __name__ == '__main__';
+	get_employee_tasks(sys.argv[1])
